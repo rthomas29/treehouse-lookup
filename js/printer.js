a65625a1904f25f156ead09b03fcd2f668e5927e
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-09-10 14:16:15 -0400

    removing unnecessary crap from printer.js

--- js/printer.js
+++ js/printer.js
@@ -1,11 +1,5 @@
-const printMessage = (username, badgeCount, points, category) => {
-  const message = `${username} has ${badgeCount} total badge(s) ${points} points in ${category} .`;
-  console.log(message);
-};
-
 const printErrorMessage = (error) => {
   console.error(error.message);
 };
 
-module.exports.print = printMessage;
 module.exports.printErr = printErrorMessage;
