387457cdaed21f1c360aa88fd022f5312f0b538b
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-09-22 12:48:50 -0400

    changed optional PORT to 5000

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const routes = require('./js/routes');
+const express = require('express')
+const routes = require('./js/routes')
 
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 5000
 
-app.use('/', routes);
-app.use(express.static('public'));
-app.set('view engine', 'pug');
+app.use('/', routes)
+app.use(express.static('public'))
+app.set('view engine', 'pug')
 
 app.listen(port, () => {
-  console.log('up and running');
-});
+  console.log('up and running')
+})
