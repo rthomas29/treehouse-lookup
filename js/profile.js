a95f345d4363604a89426d73867b3acd6b414464
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-09-22 12:34:27 -0400

    removing dead code

--- js/profile.js
+++ js/profile.js
@@ -1,41 +1,40 @@
-const https = require('https');
-const printLib = require('./printer.js');
+const https = require('https')
 
 module.exports = {
   get: (myName, req, res) => {
     try {
-      https.get(`https://teamtreehouse.com/${myName}.json`, (response) => {
-        response.setEncoding('utf8');
-        let body = '';
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
+      https.get(`https://teamtreehouse.com/${myName}.json`, response => {
+        response.setEncoding('utf8')
+        let body = ''
+        response.on('data', chunk => {
+          body += chunk
+        })
         response.on('end', () => {
           if (response.statusCode === 200) {
             try {
-              const profile = JSON.parse(body);
-              const points = profile.points;
-              const badges = profile.badges;
-              let imageSrc = 'https://achievement-images.teamtreehouse.com/treehouse.png';
+              const profile = JSON.parse(body)
+              const points = profile.points
+              const badges = profile.badges
+              let imageSrc = 'https://achievement-images.teamtreehouse.com/treehouse.png'
               if (profile.gravatar_url) {
-                imageSrc = profile.gravatar_url;
+                imageSrc = profile.gravatar_url
               }
               for (const key in points) {
                 if (points[key] === 0) {
-                  delete points[key];
+                  delete points[key]
                 }
               }
-              res.render('data', { points, myName, imageSrc, badges });
+              res.render('data', { points, myName, imageSrc, badges })
             } catch (error) {
-              printLib.printErr(error);
+              console.error(error)
             }
           } else {
-            res.render('error', { response });
+            res.render('error', { response })
           }
-        });
-      });
+        })
+      })
     } catch (error) {
-      printLib.print(error);
+      console.error(error)
     }
   },
-};
+}
