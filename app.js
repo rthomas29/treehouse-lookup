77d29a249d0caf1c54fc0ae39de4ee8a1ce03c84
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-09-22 12:56:06 -0400

    env variable fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./js/routes')
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.use('/', routes)
 app.use(express.static('public'))
