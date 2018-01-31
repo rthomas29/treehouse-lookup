4c6511e08e7b72c804de1fc70cc843d14e8f7498
Rakeem <thomas.rakeem.29@gmail.com>
2018-01-31 11:10:11 -0500

    still messing around with layout

--- lookup/src/Input.js
+++ lookup/src/Input.js
@@ -4,29 +4,29 @@ import './Input.css';
 class Input extends Component {
   render() {
     return (
-      <section id="landing" className="">
-        <aside>
-          <h5>Treehouse Lookup</h5>
-        </aside>
-        <div className="d-inline-flex flex-row justify-content-center" id="flex-container">
-          <div className="form">
+      <section>
+        <header className="text-center">
+          <h1 className="">Treehouse Lookup</h1>
+        </header>
+        <div className="d-flex flex-row justify-content-around align-items-center" id="">
+          <div className="form text-center">
             <form onSubmit={this.props.submit}>
               <input
                 type="text"
-                placeholder="Enter username"
+                placeholder="Find your profile"
                 onChange={this.props.change}
                 value={this.props.name}
                 required
+                autoFocus
               />
-              <input type="submit" value="Search" />
+              <input className="btn btn-primary" type="submit" value="Search" />
             </form>
           </div>
           <div className="landing-text">
-            <p>
+            <aside className="lead">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, cum aliquam? Nemo est, inventore maxime
-              consequatur obcaecati perferendis, animi a ullam voluptatem dolor iure reiciendis nobis similique corporis
-              suscipit veritatis.
-            </p>
+              consequatur obcaecati perferendis, animi a ullam voluptatem.
+            </aside>
           </div>
         </div>
       </section>
