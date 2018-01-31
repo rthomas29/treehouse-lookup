1032e4c2db15ced9bac00fd7bcbd1ff08ecea2bb
Rakeem <thomas.rakeem.29@gmail.com>
2018-01-31 10:20:05 -0500

    updating landing page using flex

--- lookup/src/Input.js
+++ lookup/src/Input.js
@@ -4,19 +4,32 @@ import './Input.css';
 class Input extends Component {
   render() {
     return (
-      <div id="search-div" className="w-50 p-3 float-left">
-        <h5>Treehouse Lookup</h5>
-        <form onSubmit={this.props.submit}>
-          <input
-            type="text"
-            placeholder="Enter username"
-            onChange={this.props.change}
-            value={this.props.name}
-            required
-          />
-          <input type="submit" value="Search" />
-        </form>
-      </div>
+      <section id="landing" className="">
+        <aside>
+          <h5>Treehouse Lookup</h5>
+        </aside>
+        <div className="d-inline-flex flex-row justify-content-center" id="flex-container">
+          <div className="form">
+            <form onSubmit={this.props.submit}>
+              <input
+                type="text"
+                placeholder="Enter username"
+                onChange={this.props.change}
+                value={this.props.name}
+                required
+              />
+              <input type="submit" value="Search" />
+            </form>
+          </div>
+          <div className="landing-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, cum aliquam? Nemo est, inventore maxime
+              consequatur obcaecati perferendis, animi a ullam voluptatem dolor iure reiciendis nobis similique corporis
+              suscipit veritatis.
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
